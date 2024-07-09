0ba18d77b527b5435ca8457c1c7b29e9ad972c75
Andrey Skubakov <skubakov_a@mail.ru>
2024-07-09 12:11:44 +0400

    Fixed scroll and icons

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let btnOpen = document.querySelector('.feedback');
 let btnClose = document.querySelector('.btn');
 let menu = document.querySelector('.modal');
+let body = document.querySelector('body');
 
 btnOpen.onclick = function () {
     menu.classList.remove("modal-display");
+    body.classList.add("body");
 }
 
 btnClose.onclick = function () {
     menu.classList.toggle("modal-display");
+    body.classList.toggle("body");
 }
